558c83998795c85f1cd52408364a339542e9e0ba
Greenpat <patricklyngrutz@gmail.com>
2018-04-22 13:49:51 -0400

    will need some refactoring

--- rule_applicator.R
+++ rule_applicator.R
@@ -1,3 +1,8 @@
+# TODO:
+# This approach needs refactoring... given a rule "!A|B|C" need to break apart
+# into OR(A|B|C), flatten to single result, then apply negation across rules,
+# then finally return the first fitting rule.
+
 # This ruleset applicator takes a dataframe with categorical data as well as a
 # "ruleset" dataframe and returns the first matching rule along with some
 # additional label. It supports negative rules as well as concatenated values.
@@ -16,11 +21,11 @@ df <- data.frame(
 )
 
 rules <- data.frame(stringsAsFactors = F,
-    'Carbohydrate' = character(),
-    'Greens' = character(),
-    'Protein' = character(),
-    'Dressing' = character(),
-    'Recipe' = character()
+                    'Carbohydrate' = character(),
+                    'Greens' = character(),
+                    'Protein' = character(),
+                    'Dressing' = character(),
+                    'Recipe' = character()
 )
 
 rules[1,] <- c('!Crouton','Arugula','Beans','Balsamic','Healthy Greens')
@@ -29,6 +34,7 @@ rules[3,] <- c('Pita','Kale','!Shrimp|Chicken','Bleu','Veggie Special')
 rules[4,] <- c('Rice','Spinach','Shrimp','Russian','Seafood')
 
 record <- df[1,]
+record[] <- c('Pita','Kale','Tofu','Bleu')
 delim = '|'
 negation = '!'
 
@@ -38,19 +44,39 @@ rule_applicator(rules = rules, delim = '|', negation = '!'){
     attr(rules, 'rulecols') <- intersect(colnames(rules), colnames(record))
     
     # Generate keys to eventually report back which rule was triggered
-    rownames(rules) <- do.call(function(...) paste(..., sep='.:.'), rules[,attr(rules, 'rulecols')])
+    #attr(rules, 'keydict') <- 
     
-    # Create negation columns 
+    # Row splitter utility - to transform input rules table
+    # Equivalent to tidyr::unnest()
+    parsedf <- function(rules = rules, col_to_parse, delim = delim){
+        s = strsplit(rules[[col_to_parse]], split=paste0('\\',delim))
+        rules = rules[rep(rownames(rules), sapply(s, length)),]
+        rules[[col_to_parse]] = unlist(s)
+        rules
+    }
+    
+    # Create negation columns and parse by delimiter
     for (n in attr(rules, 'rulecols')){
+        
+        # Split '!' into separate column for logic
         rules[[paste0(n,'_N')]] <- grepl(negation,rules[[n]])
         rules[[n]] <- sub(negation,'',rules[[n]])
+        
+        # Parse by delim (into multiple rows)
+        rules <- parsedf(rules, n, delim)
     }
     
-    parsedf <- function(rules = rules, col_to_parse, delim = '|'){
-        s = strsplit(rules[[col_to_parse]], split=paste0('\\',delim)
-        return()
+    eval_df <- rules
+    
+    for (n in attr(rules, 'rulecols')){
+        eval_df[[paste0(n,'_O')]] <- logical(1L)
     }
     
-    lapply(attr(rules, 'rulecols'), FUN = function(n) rules[[n]]==record[[n]])
+    # Per record, this will return which rules were matched (OR logic)
+    record_eval_or <-
+    lapply(attr(rules, 'rulecols'), FUN = function(n) {
+        rules[[n]]==record[[n]]
+    })
+    record_eval_or
     
 }
